6f5b2225c178656c0936a4281d6daa9243d47aab
Levik Naparstek <levik010@gmail.com>
2019-03-18 21:55:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name
-  @@songs = 0
+ @@song_count = 0
   def initialize(name)
     @name = name
     @songs = []
@@ -11,13 +11,13 @@ class Artist
   def add_song(song)
     self.songs << song
     song.artist = self 
-    @@songs += 1
+    @@song_count += 1
   end
   def add_song_by_name(song_name)
     song_name = Song.new(song_name)
     song.artist = self 
     self.songs << song
-    @@songs +=1
+    @@song_count +=1
     
   end
 end
